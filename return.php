9c408b47f79735c2e797c993ee3500e9530e9af9
Mayara Silva <devmayara11@gmail.com>
2022-07-28 11:39:54 -0300

    Estrutural Getall

--- return.php
+++ return.php
@@ -2,6 +2,6 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Content-Type: application/json");
+header("Content-Type: application/json; charset=UTF-8");
 echo json_encode($array);
 exit;
